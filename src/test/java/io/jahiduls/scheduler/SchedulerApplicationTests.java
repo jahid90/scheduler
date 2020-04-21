480ce669ba472f020830a459455e9f4827bbc1af
Jahid <s.jahid.90@gmail.com>
2020-04-21 16:42:40 +0530

    initial setup

---
+++ src/test/java/io/jahiduls/scheduler/SchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.jahiduls.scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
